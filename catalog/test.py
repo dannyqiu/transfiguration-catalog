103a198d25a09d1bf6286789efb506418766f7b6
Danny Qiu <dqiu55@gmail.com>
2014-08-30 14:17:47 -0400

    Improve test page to show all book info

---
+++ catalog/test.py
@@ -0,0 +1,21 @@
+import os, webapp2, jinja2
+from catalog import JINJA_ENVIRONMENT
+from functions import BookLists
+
+class TestPage(webapp2.RequestHandler):
+
+    global bookLists
+    bookLists = BookLists()
+
+    def get(self):
+        books = bookLists.get_books_values()[10:]
+        books = [[books[r][c] for c in range(0,5)] for r in range(0,len(books))]
+        big_books = bookLists.get_big_books_values()[1:]
+        big_books = [[big_books[r][c] for c in range(0,5)] for r in range(0,len(big_books))]
+
+        template_values = {
+            'books': books,
+            'big_books': big_books
+        }
+        template = JINJA_ENVIRONMENT.get_template('test.html')
+        self.response.write(template.render(template_values))
--- catalog/views.py
+++ catalog/views.py
@@ -4,27 +4,13 @@ from cron import *
 from catalog import JINJA_ENVIRONMENT
 from search import SearchForm
 from borrow import BorrowForm
+from test import TestPage
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render({}))
-
-class TestPage(webapp2.RequestHandler):
-
-    global bookLists
-    bookLists = BookLists()
-
-    def get(self):
-        template_values = {
-            'barcodes': bookLists.get_books_barcodes(),
-            'big_book_barcodes': bookLists.get_big_books_barcodes()
-            #'barcodes': get_books_spreadsheet(),
-            #'big_book_barcodes': get_big_books_spreadsheet()
-        }
-        template = JINJA_ENVIRONMENT.get_template('test.html')
-        self.response.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([
     ('/search', SearchForm),
