c7e01f49c70e8c57ba6153e4d9ebf03c1e157aa7
Danny Qiu <dqiu55@gmail.com>
2015-03-03 23:00:45 -0500

    Trim the empty rows at end of book lists

--- catalog/functions.py
+++ catalog/functions.py
@@ -1,9 +1,20 @@
+import logging
 from time import time, clock
 from credentials import email, password
 from constants import BOOKS_OFFSET, BIG_BOOKS_OFFSET
 import gspread
 
+def trim_list_end(book_list):
+    i = len(book_list)
+    while i > 0:
+        i -= 1
+        book_row = book_list[i]
+        if book_row[0].strip() != "" or book_row[1].strip() != "" or book_row[4].strip() != "": # Remove element if barcode, title and sticker don't exist
+            break
+    return book_list[:i+1]
+
 class BookLists:
+
     def __init__(self):
         global books
         global big_books
@@ -37,12 +48,16 @@ class BookLists:
         global updated_book_time
         global updated_big_book_time
         if books.updated != updated_book_time:
+            logging.info("Books update time: " + updated_book_time)
             books_values = books.get_all_values()[BOOKS_OFFSET:]
+            books_values = trim_list_end(books_values)
             updated_book_time = books.updated
         if big_books.updated != updated_big_book_time:
+            logging.info("Big books update time: " + updated_big_book_time)
             big_books_values = big_books.get_all_values()[BIG_BOOKS_OFFSET:]
-            updated_big_book_time = books.updated
-        print "Initialized Book Lists in " + str(clock() - start) + " seconds."
+            big_books_values = trim_list_end(big_books_values)
+            updated_big_book_time = big_books.updated
+        logging.info("Initialized Book Lists in " + str(clock() - start) + " seconds.")
 
     def get_books(self):
         return books
